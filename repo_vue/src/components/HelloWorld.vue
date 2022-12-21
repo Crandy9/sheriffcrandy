5edd6ee53b66b39f561478776b0bda241f3c91a9
Linden Crandall <lindencrandall123@gmail.com>
2022-12-21 15:46:22 +0900

    padding on div

--- repo_vue/src/components/HelloWorld.vue
+++ repo_vue/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
 
-  <div class="hello" style="color:green">
+  <!-- give some padding to keep it under transparent navbar -->
+  <div class="hello" style="color:green; padding-top: 2rem;">
     <!-- coming from HomeView.vue -->
     <h1>{{ msg }}</h1>
     <p>
