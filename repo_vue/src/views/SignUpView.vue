1d7bdd3b3310502101ab7c6585dbeb59c6773a93
Linden Crandall <lindencrandall123@gmail.com>
2023-02-15 14:10:46 +0900

    login/signup styling

--- repo_vue/src/views/SignUpView.vue
+++ repo_vue/src/views/SignUpView.vue
@@ -1,83 +1,91 @@
 <template>
-    <section class="my-signup-section">
-        <h1 class="title">
-            Sign up cuz!
-        </h1>
-        <!-- sign up form prevent default action -->
-        <form @submit.prevent="submitForm">
-            <div class="field">
-                <!-- username errors-->
-                <div v-if="errors.usernameErrors.length">
-                    <p class="my-errors" style="color:red" v-for="error in errors.usernameErrors" v-bind:key="error">
-                    <span style="color:red !important">*</span> {{ error }}
-                    </p>
+    <section class="my-login-signup-section">
+        <div class="page-sign-up">
+            <div class="columns">
+                <div class="column is-6 is-offset-3">
+                    <h1 class="title">
+                        Sign up
+                    </h1>
+                    <!-- sign up form prevent default action -->
+                    <form @submit.prevent="submitForm">
+                        <div class="field">
+                            <!-- username errors-->
+                            <div v-if="errors.usernameErrors.length">
+                                <p class="my-errors" style="color:red" v-for="error in errors.usernameErrors" v-bind:key="error">
+                                <span style="color:red !important">*</span> {{ error }}
+                                </p>
+                            </div>
+                            <!-- username -->
+                            <label for="">Username</label>
+                            <div class="control">
+                                <!-- v-model connects the data var defined below -->
+                                <input type="text" class="input" v-model="username">
+                            </div>
+                            <!-- email errors-->
+                            <div v-if="errors.emailErrors.length">
+                                <p class="my-errors" style="color:red" v-for="error in errors.emailErrors" v-bind:key="error">
+                                <span style="color:red !important">*</span> {{ error }}
+                                </p>
+                            </div>
+                            <!-- email -->
+                            <label for="">Email Address</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="email">
+                            </div>
+                            <!-- password errors-->
+                            <div v-if="errors.passwordErrors.length">
+                                <p class="my-errors" style="color:red" v-for="error in errors.passwordErrors" v-bind:key="error">
+                                <span style="color:red !important">*</span> {{ error }}
+                                </p>                        
+                            </div>
+                            <!-- password use click.prevent to prevent form submission action -->
+                            <label for="">Password</label>
+                            <div class="field has-addons">
+                                <div class="control is-expanded">
+                                    <input v-if="showPassword" type="text" class="input" v-model="password" />
+                                    <input v-else type="password" class="input" v-model="password">
+                                </div>
+                                <div class="control">
+                                    <button class="button" @click.prevent="toggleShowPassword"><span class="icon is-small is-right">
+                                        <i class="fas" :class="{ 'fa-eye-slash': showPassword, 'fa-eye': !showPassword }"></i>
+                                        </span>
+                                    </button>
+                                </div>
+                            </div>  
+                            <!-- re-enter password errors-->
+                            <div class="my-errors" v-if="errors.re_enter_passwordErrors.length">
+                                <p style="color:red" v-for="error in errors.re_enter_passwordErrors" v-bind:key="error">
+                                <span style="color:red !important">*</span> {{ error }}
+                                </p>                           
+                            </div>
+                            <!--re-enter password use click.prevent to prevent form submission action -->
+                            <label for="">Re-enter Password</label>
+                            <div class="field has-addons">
+                                <div class="control is-expanded">
+                                    <input v-if="showReEnterPassword" type="text" class="input" v-model="re_enter_password" />
+                                    <input v-else type="password" class="input" v-model="re_enter_password">
+                                </div>
+                                <div class="control">
+                                    <button class="button" @click.prevent="toggleShowReEnterPassword"><span class="icon is-small is-right">
+                                        <i class="fas" :class="{ 'fa-eye-slash': showReEnterPassword, 'fa-eye': !showReEnterPassword }"></i>
+                                        </span>
+                                    </button>
+                                </div>
+                            </div>  
+                            <!-- submit form -->
+                            <div class="field">
+                                <div class="control">
+                                    <button class="button login-signup-button">Create Account</button>
+                                </div>
+                            </div>
+                            <p class="signup-login-reroute">
+                                Already have an account? <a style="color:aqua !important; text-decoration:underline;" href="/login">Log in!</a>
+                            </p>
+                        </div>
+                    </form>
                 </div>
-                <!-- username -->
-                <label for="">Username</label>
-                <div class="control">
-                    <!-- v-model connects the data var defined below -->
-                    <input type="text" class="input" v-model="username">
-                </div>
-                <!-- email errors-->
-                <div v-if="errors.emailErrors.length">
-                    <p class="my-errors" style="color:red" v-for="error in errors.emailErrors" v-bind:key="error">
-                    <span style="color:red !important">*</span> {{ error }}
-                    </p>
-                </div>
-                <!-- email -->
-                <label for="">Email Address</label>
-                <div class="control">
-                    <input type="text" class="input" v-model="email">
-                </div>
-                <!-- password errors-->
-                <div v-if="errors.passwordErrors.length">
-                    <p class="my-errors" style="color:red" v-for="error in errors.passwordErrors" v-bind:key="error">
-                    <span style="color:red !important">*</span> {{ error }}
-                    </p>                        
-                </div>
-                <!-- password -->
-                <label for="">Password</label>
-                <div class="field has-addons">
-                    <div class="control is-expanded">
-                        <input v-if="showPassword" type="text" class="input" v-model="password" />
-                        <input v-else type="password" class="input" v-model="password">
-                    </div>
-                    <div class="control">
-                        <button class="button" @click="toggleShowPassword"><span class="icon is-small is-right">
-                            <i class="fas" :class="{ 'fa-eye-slash': showPassword, 'fa-eye': !showPassword }"></i>
-                            </span>
-                        </button>
-                    </div>
-                </div>  
-                <!-- re-enter password errors-->
-                <div class="my-errors" v-if="errors.re_enter_passwordErrors.length">
-                    <p style="color:red" v-for="error in errors.re_enter_passwordErrors" v-bind:key="error">
-                    <span style="color:red !important">*</span> {{ error }}
-                    </p>                           
-                </div>
-                <!--re-enter password -->
-                <label for="">Re-enter Password</label>
-                <div class="field has-addons">
-                    <div class="control is-expanded">
-                        <input v-if="showReEnterPassword" type="text" class="input" v-model="re_enter_password" />
-                        <input v-else type="password" class="input" v-model="re_enter_password">
-                    </div>
-                    <div class="control">
-                        <button class="button" @click="toggleShowReEnterPassword"><span class="icon is-small is-right">
-                            <i class="fas" :class="{ 'fa-eye-slash': showReEnterPassword, 'fa-eye': !showReEnterPassword }"></i>
-                            </span>
-                        </button>
-                    </div>
-                </div>  
-                <!-- submit form -->
-                <div class="field">
-                    <div class="control">
-                        <button class="button is-dark">Sign Up</button>
-                    </div>
-                </div>
-                Already have an account? <a href="/login">Log in!</a>
             </div>
-        </form>
+        </div>
     </section>
 </template>
       
@@ -108,7 +116,7 @@ export default {
     },
     methods: {
         submitForm() {
-
+            console.log('submitform clicked')
             // reset errors
             this.errors.usernameErrors = []
             this.errors.emailErrors = []
