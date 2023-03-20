9dd45cc96c68a315b45be8428f5ee69d23aabe18
Linden Crandall <lindencrandall123@gmail.com>
2023-03-20 11:04:37 +0900

    changed logout scheme

--- repo_vue/src/views/LogOutView.vue
+++ repo_vue/src/views/LogOutView.vue
@@ -10,32 +10,30 @@ import { toast } from 'bulma-toast'
 export default {
     name: 'LogOut',
 
-    mounted() {
-
+    beforeCreate() {
         // log user out
         axios.post(process.env.VUE_APP_LOGOUT_USER_API_URL, null, {headers: { 'Authorization': `Token ${this.$store.state.sf_auth_bearer}`}})
         .then(response => {
-        // handle success
-        console.log(response.data.success);
-        console.log("successfully logged user out and deleted user's auth token")
-        // remove token
-        axios.defaults.headers.common["Authorization"] = ""
-        localStorage.removeItem('sf_auth_bearer')
-        this.$store.commit('removeToken')
-        // empty cart (or save it for the user's web token)
-        this.$store.commit('clearCart')
-        // add toast message
-        toast({
-            message: this.$t('modals.logout'),
-            type: 'is-success',
-            dismissible: true,
-            pauseOnHover: true,
-            duration: 3000,
-            position: 'top-center',
-            animate: { in: 'fadeIn', out: 'fadeOut' },
-        })
-        // redirect to homepage
-        this.$router.push('/')
+          // handle success
+          console.log(response.data.success);
+          console.log("successfully logged user out and deleted user's auth token")
+          // remove token
+          axios.defaults.headers.common["Authorization"] = ""
+          localStorage.removeItem('sf_auth_bearer')
+          this.$store.commit('removeToken')
+          // empty cart (or save it for the user's web token)
+          this.$store.commit('clearCart')
+          // add toast message
+          toast({
+              message: this.$t('modals.logout'),
+              type: 'is-success',
+              dismissible: true,
+              pauseOnHover: true,
+              duration: 3000,
+              position: 'top-center',
+              animate: { in: 'fadeIn', out: 'fadeOut' },
+          })
+          this.$router.push('/')
         })
         .catch(error => {
         // handle error
@@ -48,4 +46,5 @@ export default {
         });
     }
 }
+
 </script>
