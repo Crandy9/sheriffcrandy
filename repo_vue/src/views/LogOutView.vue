69471d82f699bd1f6ad625322a463370213f6ab6
Linden Crandall <lindencrandall123@gmail.com>
2023-04-24 14:08:31 +0900

    remove pfp on logout

--- repo_vue/src/views/LogOutView.vue
+++ repo_vue/src/views/LogOutView.vue
@@ -18,25 +18,27 @@ export default {
         // log user out, pass in the cart to save it to user
         axios.post(process.env.VUE_APP_LOGOUT_USER_API_URL, cartData , {headers: { 'Authorization': `Token ${this.$store.state.sf_auth_bearer}`}})
         .then(response => {
-          // handle success
-          // remove token
-          axios.defaults.headers.common["Authorization"] = ""
-          localStorage.removeItem('sf_auth_bearer')
-          this.$store.commit('removeToken')
-          // empty cart (or save it for the user's web token)
-          this.$store.commit('clearCart')
-          this.$store.commit('clearPurchasedTrackList')
+            // handle success
+            // remove token
+            axios.defaults.headers.common["Authorization"] = ""
+            localStorage.removeItem('sf_auth_bearer')
+            this.$store.commit('removeToken')
+            // empty cart (or save it for the user's web token)
+            this.$store.commit('clearCart')
+            this.$store.commit('clearPurchasedTrackList')
+            // remove  pfp
+            this.$store.state.profile_pic_background_img = null
 
-          // add toast message
-          toast({
-              message: this.$t('modals.logout'),
-              type: 'is-success',
-              dismissible: true,
-              pauseOnHover: true,
-              duration: 2000,
-              position: 'center',
-              animate: { in: 'fadeIn', out: 'fadeOut' },
-          })
+            // add toast message
+            toast({
+                message: this.$t('modals.logout'),
+                type: 'is-success',
+                dismissible: true,
+                pauseOnHover: true,
+                duration: 2000,
+                position: 'center',
+                animate: { in: 'fadeIn', out: 'fadeOut' },
+            })
 
             this.$router.push('/')
         })
